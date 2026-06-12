3a9f8bf6f764032a1d6a70fd5a3de3904ffe1ce9
abdyrakhmanov.arzybek <abdyrakhmanov.arzybek@gmail.com>
2026-06-12 20:16:19 +0600

    added lesson6

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("Helo Test 111111")
+import random
+
+class Hero:
+    def __init__(self, name):
+        self.name = name
+
+add_test = 12 + 12
+
+def add(a,b):
+    return a + b
+
+__all__ = (
+    'Hero',
+    'add_test',
+    'add'
+)
