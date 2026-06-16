de606f4dcd9ed2ce4ad55870178120b0531044e6
Арзыбек <ardager@MacBook-Pro-Arzybek.local>
2026-06-16 20:36:54 +0600

    added lesson7

--- main.py
+++ main.py
@@ -1,39 +1,55 @@
-# # # import test
-# # from test import *
+# # # # import test
+# # # from test import *
+# # #
+# # # ardager = Hero('Ardager')
+# # #
+# # # print(ardager.name)
+# # # print(add(12, 12))
+# # # print(random.randint(1, 10))
 # #
-# # ardager = Hero('Ardager')
 # #
-# # print(ardager.name)
-# # print(add(12, 12))
-# # print(random.randint(1, 10))
+# #
+# # from my_package import Hero, add_test, add
+# # # import  random
+# # # import  sqlite3
+# #
+# #
+# #
+# # print("TEXT")yle
+#
+# # from colorama import Fore, Back, Style
+# # print(Fore.RED + 'some red text')
+# # print(Back.GREEN + 'and with a green background')
+# # print(Style.DIM + 'and in dim text')
+# # print(Style.RESET_ALL)
+# # print('back to normal now')
+#
+# import random
+# from tenacity import retry
 #
 #
+# @retry
+# def do_something_unreliable():
+#     if random.randint(0, 10) > 1:
+#         raise IOError("Broken sauce, everything is hosed!!!111one")
+#     else:
+#         return "Awesome sauce!"
 #
-# from my_package import Hero, add_test, add
-# # import  random
-# # import  sqlite3
+#
+# print(do_something_unreliable())
+
+def get_item(my_list, target_index):
+    return my_list[target_index]
 #
 #
-#
-# print("TEXT")yle
-
-# from colorama import Fore, Back, Style
-# print(Fore.RED + 'some red text')
-# print(Back.GREEN + 'and with a green background')
-# print(Style.DIM + 'and in dim text')
-# print(Style.RESET_ALL)
-# print('back to normal now')
-
-import random
-from tenacity import retry
+# print(get_item([1,2,3,4,5,6], 4))
 
 
-@retry
-def do_something_unreliable():
-    if random.randint(0, 10) > 1:
-        raise IOError("Broken sauce, everything is hosed!!!111one")
-    else:
-        return "Awesome sauce!"
+def o_n(my_list, target):
+    for i in my_list:
+        if i == target:
+            return i
+        else:
+            return 0
 
-
-print(do_something_unreliable())
+o_n([4,5], 4)
